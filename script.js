b06ac730c9310c9b1907c44dda6c77894d2fdc14
AA <aa>
2021-07-16 14:38:16 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+var msg1 = document.getElementById("message1");
+var msg2 = document.getElementById("message2");
+var msg3 = document.getElementById("message3");
+
+
+var answer = Math.floor(Math.random()*100) + 1;
+var no_of_guesses = 0;
+var tries = 10;
+var guessed_nums = [];
+
+
+ 
+function play() {
+    var user_guess = document.getElementById("guess").value;
+        if (user_guess < 1 || user_guess > 100) {
+            alert("Please enter a number between 1 and 100.");
+
+        }else{
+            guessed_nums.push(user_guess);
+            no_of_guesses+=1;
+
+            if(user_guess < answer) {
+                msg1.textContent = "Your guess is too low.";
+                msg2.textContent = "No. of guesses: " + no_of_guesses;
+                msg3.textContent = "Guessed numbers are: " + guessed_nums;
+            }
+            else if(user_guess > answer) {
+                msg1.textContent = "Your guess is too high.";
+                msg2.textContent = "No. of guesses: " + no_of_guesses;
+                msg3.textContent = "Guessed numbers are: " + guessed_nums;
+            }
+            else if(user_guess == answer){
+                msg1.textContent = "Eureka! You Did IT!";
+                msg2.textContent = "The number was: " + answer;
+                msg3.textContent = "You guessed it in " + no_of_guesses + " guesses";
+                document.getElementById("my_btn").disabled = true; 
+            }
+            tries -=1;
+        }
+        if (tries === 0) {
+            msg1.textContent = "Unfortunately you've exeeded the number of tries";
+            msg2.textContent = "The number was: " + answer;
+            msg3.textContent = "You've tried to guess it in " + no_of_guesses + " guesses";
+            document.getElementById("my_btn").disabled = true;
+            }
+    }
+
+
+
+          
+document.getElementById("reset").onclick = function() {
+    location.reload();
+ };
+   
+
+
+
+  
